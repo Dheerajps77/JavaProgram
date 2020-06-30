ebf3ab8b612488abe35b203247bf71d8a6d47660
Dheeraj Pratap Singh <dheerajps77@gmail.com>
2020-06-30 21:34:05 +0530

    new

--- src/Program30June/ToFindIndexOfElement.java
+++ src/Program30June/ToFindIndexOfElement.java
@@ -1,26 +1,30 @@
 package Program30June;
 
+import java.util.Arrays;
+
 public class ToFindIndexOfElement {
 	
 	public static void toFindIndexOfArray()
 	{
 		try {
-			int n[] = {3,4,5,9,7,6,4,7, 8};
+			int n[] = {3,4,5,9,7,6,4};			
 			int values=n.length;
 			double d = 0;
-			for(int i=0;i<values;i++)
+			for(int i=0;i<values;)
 			{
-				if(n.length%2==0)
+				if(values%2==0)
 				{
 					int n1=n[values/2];
 					int n2=n[values/2-1];
 					int sum=n1+n2;
 					
 					d=((double)(sum))/2;
+					break;
 				}
 				else
 				{
-					d=(double)n[n.length/2];
+					d=(double)n[values/2];
+					break;
 				}
 			}
 			System.out.println(d);
