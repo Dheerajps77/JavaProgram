fffcca22ba77c3c9ef587f6835e38460b901d4e1
Dheerajps77 <39496037+Dheerajps77@users.noreply.github.com>
2025-02-17 17:51:17 +0530

    Update DistinctValue.java

--- src/Aug2024/DistinctValue.java
+++ src/Aug2024/DistinctValue.java
@@ -8,141 +8,122 @@ import java.util.Set;
 
 public class DistinctValue {
 
-	public static void distinctDouble() {
-		try {
-			double[] values = { 1.1, 2.2, 3.3, 1.1, 4.4, 2.2, 5.5, 3.3, 5.0, 42.43 };
-
-			Map<Double, Integer> maps = new HashMap<Double, Integer>();
-
-			for (int i = 0; i < values.length; i++) {
-				if (maps.get(values[i]) != null) {
-					maps.put(values[i], maps.get(values[i]) + 1);
-				} else {
-					maps.put(values[i], 1);
-				}
-			}
-
-			for (Map.Entry<Double, Integer> entry : maps.entrySet()) {
-				System.out.println(entry.getKey() + " : " + entry.getValue());
-			}
-
-		} catch (Exception e) {
-			throw e;
-		}
-	}
-
-	public static void distinctfloat() {
-		try {
-			float[] values = { 1.1f, 2.2f, 3.3f, 1.1f, 4.4f, 2.2f, 5.5f, 3.3f, 444.5f };
-
-			Map<Float, Integer> maps = new HashMap<Float, Integer>();
-
-			for (int i = 0; i < values.length; i++) {
-				if (maps.get(values[i]) != null) {
-					maps.put(values[i], maps.get(values[i]) + 1);
-				} else {
-					maps.put(values[i], 1);
-				}
-			}
-
-			for (Map.Entry<Float, Integer> entry : maps.entrySet()) {
-				System.out.println(entry.getKey() + " : " + entry.getValue());
-			}
-
-		} catch (Exception e) {
-			throw e;
-		}
-	}
-
 	public static void distinctInteger() {
-		try {
-			int num[] = { 34, 53, 34, 56, 65, 54, 2, 3, 2, 3, 4, 4, 5, 5, 76, 76, 76, 80, 80, 89, 80 };
+        try {
+            int num[] = {34, 53, 34, 56, 65, 54, 2, 3, 2, 3, 4, 4, 5, 5, 76, 76, 76, 80, 80, 89, 80};
+            Map<Integer, Integer> maps = new HashMap<>();
 
-			Map<Integer, Integer> maps = new HashMap<Integer, Integer>();
+            for (int n : num) {
+                if (maps.get(n) != null) {
+                    maps.put(n, maps.get(n) + 1);
+                } else {
+                    maps.put(n, 1);
+                }
+            }
 
-			for (int i = 0; i < num.length; i++) {
-				if (maps.get(num[i]) != null) {
-					maps.put(num[i], maps.get(num[i]) + 1);
-				} else {
-					maps.put(num[i], 1);
-				}
-			}
+            for (Entry<Integer, Integer> entry : maps.entrySet()) {
+                System.out.println(entry.getKey() + " : " + entry.getValue());
+            }
 
-			Set<Entry<Integer, Integer>> setsOfEntry = maps.entrySet();
-			Iterator<Entry<Integer, Integer>> iteratorOfSet = setsOfEntry.iterator();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-			while (iteratorOfSet.hasNext()) {
-				Entry<Integer, Integer> entrySet = iteratorOfSet.next();
-				System.out.println(entrySet.getKey() + " : " + entrySet.getValue());
-			}
+    public static void distinctDouble() {
+        try {
+            double num[] = {1.1, 2.2, 1.1, 3.3, 3.3, 3.3};
+            Map<Double, Integer> maps = new HashMap<>();
 
-		} catch (Exception e) {
-			throw e;
-		}
-	}
+            for (double n : num) {
+                if (maps.get(n) != null) {
+                    maps.put(n, maps.get(n) + 1);
+                } else {
+                    maps.put(n, 1);
+                }
+            }
 
-	public static void distinctString() {
-		try {
+            for (Entry<Double, Integer> entry : maps.entrySet()) {
+                System.out.println(entry.getKey() + " : " + entry.getValue());
+            }
 
-			String str = "I know , I know , I've heard it before. "
-					+ "The cat sat on the mat, the mat that was soft and warm. "
-					+ "She shouted , 'No , no , no!' as she ran away. "
-					+ "He kept saying , 'Please , please , I need your help."
-					+ "The rain, rain , go away , come again another day.";
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-			String newString[] = str.toLowerCase().split(" ");
+    public static void distinctFloat() {
+        try {
+            float num[] = {1.2f, 2.3f, 1.2f, 1.2f, 2.3f, 3.4f};
+            Map<Float, Integer> maps = new HashMap<>();
 
-			Map<String, Integer> maps = new HashMap<String, Integer>();
+            for (float n : num) {
+                if (maps.get(n) != null) {
+                    maps.put(n, maps.get(n) + 1);
+                } else {
+                    maps.put(n, 1);
+                }
+            }
 
-			for (int i = 0; i < newString.length; i++) {
-				if (maps.get(newString[i]) != null) {
-					maps.put(newString[i], maps.get(newString[i]) + 1);
-				} else {
-					maps.put(newString[i], 1);
-				}
-			}
+            for (Entry<Float, Integer> entry : maps.entrySet()) {
+                System.out.println(entry.getKey() + " : " + entry.getValue());
+            }
 
-			Set<Entry<String, Integer>> setss = maps.entrySet();
-			Iterator<Entry<String, Integer>> entrySetss = setss.iterator();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-			while (entrySetss.hasNext()) {
-				Entry<String, Integer> entryOne = entrySetss.next();
-				System.out.println(entryOne.getKey() + " : " + entryOne.getValue());
-			}
+    public static void distinctCharacter() {
+        try {
+            String str = "hello world";
+            Map<Character, Integer> maps = new HashMap<>();
 
-		} catch (Exception e) {
-			throw e;
-		}
-	}
+            for (char ch : str.toCharArray()) {
+                if (maps.get(ch) != null) {
+                    maps.put(ch, maps.get(ch) + 1);
+                } else {
+                    maps.put(ch, 1);
+                }
+            }
 
-	public static void distinctChar() {
-		try {
-			String str = "abcabcabcd";
-			char c[] = str.toCharArray();
+            for (Entry<Character, Integer> entry : maps.entrySet()) {
+                System.out.println(entry.getKey() + " : " + entry.getValue());
+            }
 
-			Map<Character, Integer> setss = new HashMap<Character, Integer>();
-			for (int i = 0; i < c.length; i++) {
-				if (setss.get(c[i]) != null) {
-					setss.put(c[i], setss.get(c[i]) + 1);
-				} else {
-					setss.put(c[i], 1);
-				}
-			}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-			Set<Entry<Character, Integer>> enterySetValue = setss.entrySet();
-			Iterator<Entry<Character, Integer>> iteratorOfEntry = enterySetValue.iterator();
+    public static void distinctString() {
+        try {
+            String str = "I know , I know , I've heard it before. "
+                    + "The cat sat on the mat, the mat that was soft and warm. "
+                    + "She shouted , 'No , no , no!' as she ran away. "
+                    + "He kept saying , 'Please , please , I need your help. "
+                    + "The rain, rain , go away , come again another day.";
 
-			while (iteratorOfEntry.hasNext()) {
-				Entry<Character, Integer> singleEntryFromSet = iteratorOfEntry.next();
+            String newString[] = str.toLowerCase().split(" ");
 
-				System.out.println(singleEntryFromSet.getKey() + " " + singleEntryFromSet.getValue());
-			}
-		} catch (
+            Map<String, Integer> maps = new HashMap<>();
 
-		Exception e) {
-			throw e;
-		}
-	}
+            for (String word : newString) {
+                if (maps.get(word) != null) {
+                    maps.put(word, maps.get(word) + 1);
+                } else {
+                    maps.put(word, 1);
+                }
+            }
+
+            for (Entry<String, Integer> entry : maps.entrySet()) {
+                System.out.println(entry.getKey() + " : " + entry.getValue());
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 	
 	public static void printOnlyChar()
